f91a5a8a9da246adbb190dcbb8ed688228a7420e
Adam Przybylek <adam.przybylek@gmail.com>
2021-03-18 10:54:58 +0100

    The Pair class has been replaced by ComparablePair which extends javafx.util.Pair and implements Comparable.

--- TopPopularLinks.java
+++ TopPopularLinks.java
@@ -11,12 +11,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+
 
 import java.io.IOException;
 import java.lang.Integer;
@@ -51,90 +53,101 @@ public class TopPopularLinks extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
         // TODO
+       
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
-        // TODO
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            //TODO
+       	
+        }
     }
 
     public static class LinkCountReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
-        // TODO
+        private Integer N;
+        private TreeSet<ComparablePair<Integer, Integer>> countToIdMap = new TreeSet<>();
+
+        @Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+            this.N = conf.getInt("N", 10);
+        }
+
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            //TODO
+
+        }
+        
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            for (ComparablePair<Integer, Integer> item : countToIdMap) {
+        	context.write(new IntWritable(item.getValue()), new IntWritable(item.getKey()));
+    	    }
+        }
+
     }
 
     public static class TopLinksMap extends Mapper<Text, Text, NullWritable, IntArrayWritable> {
-        Integer N;
-
+             
         @Override
-        protected void setup(Context context) throws IOException,InterruptedException {
-            Configuration conf = context.getConfiguration();
-            this.N = conf.getInt("N", 10);
+        public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+            // TODO
+     
         }
-        // TODO
+        
     }
 
     public static class TopLinksReduce extends Reducer<NullWritable, IntArrayWritable, IntWritable, IntWritable> {
-        Integer N;
-
+        private Integer N;
+        private TreeSet<ComparablePair<Integer, Integer>> countToIdMap = new TreeSet<>();
+        
         @Override
         protected void setup(Context context) throws IOException,InterruptedException {
             Configuration conf = context.getConfiguration();
             this.N = conf.getInt("N", 10);
         }
-        // TODO
+
+        @Override
+        public void reduce(NullWritable key, Iterable<IntArrayWritable> values, Context context) throws IOException, InterruptedException {
+        
+	        for (IntArrayWritable  val: values) {
+	        	IntWritable[] pair = (IntWritable[]) val.toArray();
+	        	Integer id = pair[0].get();
+	        	Integer count = pair[1].get();
+	        	countToIdMap.add(new ComparablePair<Integer, Integer>(count, id));
+	        }
+	
+	        if (countToIdMap.size() > N) {
+	        	countToIdMap.remove(countToIdMap.first());                
+	        } 
+        }
+        
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            // TODO
+      	
+        }
     }
 }
 
 // >>> Don't Change
-class Pair<A extends Comparable<? super A>,
-        B extends Comparable<? super B>>
-        implements Comparable<Pair<A, B>> {
 
-    public final A first;
-    public final B second;
+class ComparablePair<K extends Comparable<? super K>, V extends Comparable<? super V>>
+	extends javafx.util.Pair<K,V> 
+	implements Comparable<ComparablePair<K, V>> {
 
-    public Pair(A first, B second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public static <A extends Comparable<? super A>,
-            B extends Comparable<? super B>>
-    Pair<A, B> of(A first, B second) {
-        return new Pair<A, B>(first, second);
+    public ComparablePair(K key, V value) {
+	super(key, value);
     }
 
     @Override
-    public int compareTo(Pair<A, B> o) {
-        int cmp = o == null ? 1 : (this.first).compareTo(o.first);
-        return cmp == 0 ? (this.second).compareTo(o.second) : cmp;
+    public int compareTo(ComparablePair<K, V> o) {
+        int cmp = o == null ? 1 : (this.getKey()).compareTo(o.getKey());
+        return cmp == 0 ? (this.getValue()).compareTo(o.getValue()) : cmp;
     }
 
-    @Override
-    public int hashCode() {
-        return 31 * hashcode(first) + hashcode(second);
-    }
-
-    private static int hashcode(Object o) {
-        return o == null ? 0 : o.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Pair))
-            return false;
-        if (this == obj)
-            return true;
-        return equal(first, ((Pair<?, ?>) obj).first)
-                && equal(second, ((Pair<?, ?>) obj).second);
-    }
-
-    private boolean equal(Object o1, Object o2) {
-        return o1 == o2 || (o1 != null && o1.equals(o2));
-    }
-
-    @Override
-    public String toString() {
-        return "(" + first + ", " + second + ')';
-    }
 }
+
 // <<< Don't Change
